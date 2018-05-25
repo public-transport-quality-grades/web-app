3caa5e6a0f7d607a7095013fe1c74b591c98a9a5
Robin Suter <suter.robin@gmail.com>
2018-05-25 12:37:46 +0200

    integrate vector tile layer with api changes

--- src/components/VectorgridLayer.js
+++ src/components/VectorgridLayer.js
@@ -1,12 +1,13 @@
 // @flow
 
-import L from 'leaflet';
+import L, {GridLayer as LeafletGridLayer} from 'leaflet';
 import PropTypes from 'prop-types';
 import {} from 'leaflet.vectorgrid/dist/Leaflet.VectorGrid.bundled.js'
 import { GridLayer } from 'react-leaflet';
+import type { GridLayerProps } from 'react-leaflet';
 
 
-export default class VectorgridLayer extends GridLayer<{}, {}> {
+export default class VectorgridLayer extends GridLayer<LeafletGridLayer, GridLayerProps> {
     static propTypes = {
         url: PropTypes.string.required,
         opacity: PropTypes.number,
@@ -14,28 +15,27 @@ export default class VectorgridLayer extends GridLayer<{}, {}> {
         featureStyle: PropTypes.func
     };
 
-    getLayerOptions = (styleFunc: (properties: {}, zoom: number)=> {}, opacity: number) => {
+    getLayerOptions = (styleFunc: (properties: {}, zoom: number)=> {}, layerName: string, opacity: number) => {
         return {
             rendererFactory: L.canvas.tile,
             vectorTileLayerStyles: {
-                'oevgk18_20181113_Daygeojson': styleFunc
+                'oevgk18': styleFunc
             },
             maxZoom: 17,
-            maxNativeZoom: 13,
+            maxNativeZoom: 17,
             opacity: opacity
         };
     };
 
     createLeafletElement(props: Object): Object {
-        const { url, featureStyle, opacity } = props;
-        return L.vectorGrid.protobuf(url, this.getLayerOptions(featureStyle, opacity));
+        const { url, featureStyle, opacity, layerName } = props;
+        return L.vectorGrid.protobuf(url, this.getLayerOptions(featureStyle, layerName, opacity));
     }
 
     updateLeafletElement(fromProps: Object, toProps: Object) {
         super.updateLeafletElement(fromProps, toProps);
-        const {url, featureStyle, opacity} = toProps;
         if (toProps.layerKey !== fromProps.layerKey) {
-            this.leafletElement.protobuf(url, this.getLayerOptions(featureStyle, opacity))
+            this.leafletElement.setUrl(toProps.url);
         }
     }
 }
--- src/config.js
+++ src/config.js
@@ -1,5 +1,16 @@
 export const mapboxStyleUrl = "https://free.tilehosting.com/styles/positron/style.json?key=HEhIieMgsLPA7PKcbTfx";
 
+export const colorsOeVGK18 = {
+    "A": "#700038",
+    "B": "#BC42FF",
+    "C": "#9966FF",
+    "D": "#00B000",
+    "E": "#B3FF40",
+    "F": "#DEF325"
+};
+
+export const oeVGK18Opacity = 0.6;
+
 export const colorsARE = {
     "A": "#700038",
     "B": "#9966FF",
