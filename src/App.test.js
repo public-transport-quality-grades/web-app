03949f52e86e382bdc0f8822aa8e447f68207217
Robin Suter <suter.robin@gmail.com>
2018-06-03 16:21:40 +0200

    remove non-working tests

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-//import App from './App';
-
-it('renders without crashing', () => {
-    // TODO: Map crashes because there is no browser
-    // when App gets imported, it also imports mapbox-gl
-    //shallow(<App />);
-});
